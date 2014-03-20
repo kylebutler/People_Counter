0acbbbbe928b69c1b2ccb3bedaf7ab389756ee31
kylebutler <butlerk@live.com>
2014-03-20 17:31:23 -0400

    do nothing when all high or low
    have the code hang while sensors are all high or low.  Added delays
    after an enter or exit so beam doesnt get a double count when only an
    exit or enter is occurring.

--- IR_ppl_counter/IR_ppl_counter.ino
+++ IR_ppl_counter/IR_ppl_counter.ino
@@ -1,8 +1,8 @@
 #include <Time.h>
 #include <SoftwareSerial.h>
 
-int ppl_in=0;// count of people in
-int ppl_out=0;// count of people out
+int ppl_in;// count of people in
+int ppl_out;// count of people out
 int set_up=5;// time allowed for ckt to set up
 int sensor1;// first IR sensor
 int sensor2;// second IR sensor
@@ -19,7 +19,7 @@ void setup()
     Serial.print(".");
     delay(1000);
   }
-  Serial.println("\nSensor setup complete");
+  Serial.print("\nSensor setup complete");
   
 }
 
@@ -27,34 +27,20 @@ void loop()
 {
   sensor1=digitalRead(8);// assigns sensor1 to the value read at pin 8
   sensor2=digitalRead(9);// assigns sensor2 to the value read at pin 9
-
-
-    if((sensor1==LOW) && (sensor2==LOW))// no activity from sensors
-    {
-      delay(1);
-    }
   
+    if((sensor1==LOW) && (sensor2==LOW));// no activity from sensors 
     if((sensor1==HIGH) && (sensor2==LOW))// person enters room
     {      
+      while(sensor1==HIGH);
       ppl_in++;// increment count of ppl entering
       Serial.println(ppl_in);
-      //delay(100);
-      
-      while(sensor1==HIGH && sensor2==LOW)
-       {
-           sensor1=digitalRead(8);
-           sensor2=digitalRead(9);
-       }
-      Serial.print("exited loop");
-      //ADD DEBOUNCE TO INPUTS!
+      delay(200);
     }
     if((sensor1==LOW) && (sensor2==HIGH))// person exits the room
     {
+      while(sensor2==HIGH);
       ppl_out++;// increment count of ppl exiting
-      delay(100);
+      delay(200);
     }
-    if((sensor1==HIGH) && (sensor2==HIGH))
-    {
-     delay(10);// nothing needs to be done if both are high 
-    }
+    if((sensor1==HIGH) && (sensor2==HIGH));// nothing needs to be done if both are high
 }  
