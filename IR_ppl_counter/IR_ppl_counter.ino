2225787725d43756e5b5e3df513c01f33ea9c8d5
kylebutler <butlerk@live.com>
2014-05-19 22:11:39 -0600

    Code Revamp
    Totally reworked the code. Incorporated a switch statement to handle
    sensor states. Also added a flag to help better control when the sensors
    increment/decrement the ppl_in and ppl_out variables

--- IR_ppl_counter/IR_ppl_counter.ino
+++ IR_ppl_counter/IR_ppl_counter.ino
@@ -1,11 +1,13 @@
 #include <Time.h>
 #include <SoftwareSerial.h>
 
-int ppl_in;// count of people in
-int ppl_out;// count of people out
+int ppl_in=0;// count of people in
+int ppl_out=0;// count of people out
 int set_up=5;// time allowed for ckt to set up
 int sensor1;// first IR sensor
 int sensor2;// second IR sensor
+int state;// switch variable
+boolean flag=true;// flag for state of sensor 
 
 void setup()
 {
@@ -28,19 +30,51 @@ void loop()
   sensor1=digitalRead(8);// assigns sensor1 to the value read at pin 8
   sensor2=digitalRead(9);// assigns sensor2 to the value read at pin 9
   
-    if((sensor1==LOW) && (sensor2==LOW));// no activity from sensors 
-    if((sensor1==HIGH) && (sensor2==LOW))// person enters room
-    {      
-      while(sensor1==HIGH);
+  if((sensor1==LOW) && (sensor2==LOW))// no activity from sensors
+  {
+   state=0; 
+   flag=true;
+  }
+  if((sensor1==HIGH) && (sensor2==HIGH));// nothing needs to be done if both are high
+  {
+   state=0;
+   //flag=true; 
+  }
+  if((sensor1==HIGH) && (sensor2==LOW) && (flag==true))// person enters room
+  {
+   state=1; 
+  }
+  if((sensor1==LOW) && (sensor2==HIGH) && (flag==true))// person exits the room
+  {
+   state=2; 
+  }
+  
+  switch(state)
+  {
+    case(0):// do nothing for both high/low
+      state=0;
+    break;
+    
+    case(1):// person enters
       ppl_in++;// increment count of ppl entering
       Serial.println(ppl_in);
-      delay(200);
-    }
-    if((sensor1==LOW) && (sensor2==HIGH))// person exits the room
-    {
-      while(sensor2==HIGH);
+      state=0;
+      flag=false;
+    break; 
+    
+    case(2):// person exits
+      ppl_in--;// decrement count of entered ppl
       ppl_out++;// increment count of ppl exiting
-      delay(200);
-    }
-    if((sensor1==HIGH) && (sensor2==HIGH));// nothing needs to be done if both are high
+      if(ppl_in>=0)
+      {
+        Serial.println(ppl_in);
+      }
+      state=0;       
+      flag=false;     
+    break;
+    
+    default:
+      state=0;
+  }
+  //delay(300);// wait .3s before looping again
 }  
